783759f0454dfa526c2720a99378ec1669210d54
Madhushanmugam <94168215+Madhumithashanmugam@users.noreply.github.com>
2021-12-01 15:43:00 +0530

    Create test.c

---
+++ 3_Implementation/inc/test.c
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include "../i2c/i2cmaster.h"
+
+#include "mma8452.h"
+uint8_t twi_buffer[4];
+
+void mma_init(){   
+}
+
+uint8_t mma_get_PL(){
+  }
+
+uint8_t mma_get_tap(){
+  }
+
+void mma_standby(){
+}
